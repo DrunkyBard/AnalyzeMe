e7c86fbd5b6e54ec6a10981043bac3137c4f3055
DrunkyBard <sattar.imamov@gmail.com>
2015-07-28 01:08:16 +0300

    - Add Rx method analyzer - Add Analyzer for virtual method/property/event call

---
+++ src/AnalyzeMe/AnalyzeMe.Design.Analyzers/CallVirtualInConstructorAnalyzer.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Immutable;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.Diagnostics;
+
+namespace AnalyzeMe.Design.Analyzers
+{
+    [DiagnosticAnalyzer(LanguageNames.CSharp)]
+    public sealed class CallVirtualInConstructorAnalyzer : DiagnosticAnalyzer
+    {
+        public override void Initialize(AnalysisContext context)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics { get; }
+    }
+}
