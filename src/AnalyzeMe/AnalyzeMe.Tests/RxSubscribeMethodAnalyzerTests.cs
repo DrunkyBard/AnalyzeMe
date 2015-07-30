db65554026f3836277aa226a47dbc510e2d8d765
DrunkyBard <sattar.imamov@gmail.com>
2015-07-30 23:25:01 +0300

    - Add Subscribe method definition in RxSubscribeMethodAnalyzerTests

--- src/AnalyzeMe/AnalyzeMe.Tests/RxSubscribeMethodAnalyzerTests.cs
+++ src/AnalyzeMe/AnalyzeMe.Tests/RxSubscribeMethodAnalyzerTests.cs
@@ -1,5 +1,5 @@
-﻿using AnalyzeMe.Design.Analyzers;
-using AnalyzeMe.WorkProcess.Analyzers;
+﻿using System;
+using AnalyzeMe.Design.Analyzers;
 using Microsoft.CodeAnalysis;
 using Microsoft.CodeAnalysis.Diagnostics;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
@@ -13,13 +13,32 @@ namespace AnalyzeMe.Tests
         private const string Code = @"
 using System;
 
+namespace System
+{
+    public static class ObservableExtensions
+    {
+        public static IDisposable Subscribe<T>(this IObservable<T> source, Action<T> onNext)
+        { return null; }
+
+        public static IDisposable Subscribe<T>(this IObservable<T> source, Action<T> onNext, Action onCompleted)
+        { return null; }
+
+        public static IDisposable Subscribe<T>(this IObservable<T> source, Action<T> onNext, Action<Exception> onError)
+        { return null; }
+
+        public static IDisposable Subscribe<T>(this IObservable<T> source, Action<T> onNext, Action<Exception> onError, Action onCompleted)
+        { return null; }
+    }
+}
+
 namespace Test
 {
     public class Foo
     {
         public void Bar()
         {
-            Console.WriteLine(1);
+            IObservable<object> obs = null;
+            obs.Subscribe(_ => {});
         }
     }
 }
