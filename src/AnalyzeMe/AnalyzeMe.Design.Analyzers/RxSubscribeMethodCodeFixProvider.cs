31bdd36358dd01009e2eb3c133d64bdf66a6f1a6
DrunkyBard <sattar.imamov@gmail.com>
2015-08-01 20:35:14 +0300

    - Add code fix provider for RxSubscribeMethodAnalyzer

---
+++ src/AnalyzeMe/AnalyzeMe.Design.Analyzers/RxSubscribeMethodCodeFixProvider.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Immutable;
+using System.Composition;
+using System.Threading.Tasks;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CodeFixes;
+
+namespace AnalyzeMe.Design.Analyzers
+{
+    [ExportCodeFixProvider(LanguageNames.CSharp, Name = nameof(RxSubscribeMethodCodeFixProvider)), Shared]
+    public sealed class RxSubscribeMethodCodeFixProvider : CodeFixProvider
+    {
+        public sealed override ImmutableArray<string> FixableDiagnosticIds => ImmutableArray.Create(RxSubscribeMethodAnalyzer.RxSubscribeMethodDiagnosticId);
+
+        public sealed override FixAllProvider GetFixAllProvider()
+        {
+            
+            return WellKnownFixAllProviders.BatchFixer;
+        }
+
+        public override Task RegisterCodeFixesAsync(CodeFixContext context)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
