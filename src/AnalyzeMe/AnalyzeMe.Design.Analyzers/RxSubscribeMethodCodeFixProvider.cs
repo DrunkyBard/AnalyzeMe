dc5c0cd142a7d5b3e2c0a6a9df9369772321cda1
DrunkyBard <sattar.imamov@gmail.com>
2015-08-03 01:21:36 +0300

    - Beautify generated "OnError" parameter

--- src/AnalyzeMe/AnalyzeMe.Design.Analyzers/RxSubscribeMethodCodeFixProvider.cs
+++ src/AnalyzeMe/AnalyzeMe.Design.Analyzers/RxSubscribeMethodCodeFixProvider.cs
@@ -1,13 +1,13 @@
-﻿using System;
-using System.Collections.Immutable;
+﻿using System.Collections.Immutable;
 using System.Composition;
-using System.Diagnostics.Contracts;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CodeActions;
 using Microsoft.CodeAnalysis.CodeFixes;
 using Microsoft.CodeAnalysis.CSharp;
 using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.CodeAnalysis.Formatting;
 
 namespace AnalyzeMe.Design.Analyzers
 {
@@ -15,10 +15,10 @@ namespace AnalyzeMe.Design.Analyzers
     public sealed class RxSubscribeMethodCodeFixProvider : CodeFixProvider
     {
         public sealed override ImmutableArray<string> FixableDiagnosticIds => ImmutableArray.Create(RxSubscribeMethodAnalyzer.RxSubscribeMethodDiagnosticId);
+        private SyntaxTrivia WhiteSpace => SyntaxFactory.Whitespace(" ");
 
         public sealed override FixAllProvider GetFixAllProvider()
         {
-
             return WellKnownFixAllProviders.BatchFixer;
         }
 
@@ -33,41 +33,47 @@ namespace AnalyzeMe.Design.Analyzers
             var newInvocationArguments = methodArguments.Arguments.First().NameColon != null
                 ? CreateNamedArgumentsFrom(methodArguments)
                 : CreateSimpleArgumentsFrom(methodArguments);
+            var updatedRoot = root.ReplaceNode(methodArguments, newInvocationArguments.WithAdditionalAnnotations(Formatter.Annotation));
+            var updatedDocument = context.Document.WithSyntaxRoot(updatedRoot);
 
-            var a = 1;
+            context.RegisterCodeFix(
+                CodeAction.Create("Add onError parameter.", c => Task.FromResult(updatedDocument), "AddSubscribeOnErrorParameter"),
+                diagnostic);
         }
 
         private ArgumentListSyntax CreateNamedArgumentsFrom(ArgumentListSyntax oldArguments)
         {
             var onErrorNameColon = SyntaxFactory.NameColon("onError");
             var nopToken = SyntaxFactory.Token(SyntaxKind.None);
-            var parameter = SyntaxFactory.Parameter(SyntaxFactory.Identifier("e"));
+            var parameter = SyntaxFactory
+                .Parameter(SyntaxFactory.Identifier("e"))
+                .WithLeadingTrivia(SyntaxFactory.Whitespace(" "));
+            var lambdaBodyToken = SyntaxFactory.Token
+                (
+                    SyntaxFactory.TriviaList(),
+                    SyntaxKind.OpenBraceToken,
+                    SyntaxFactory.TriviaList
+                        (
+                            WhiteSpace,
+                            SyntaxFactory.Comment
+                                (
+                                    @"/*TODO: handle this!*/"
+                                ),
+                            WhiteSpace
+                        )
+                );
             var lambdaBody = SyntaxFactory.Block()
-                                                    .WithOpenBraceToken
-                                                    (
-                                                        SyntaxFactory.Token
-                                                        (
-                                                            SyntaxFactory.TriviaList(),
-                                                            SyntaxKind.OpenBraceToken,
-                                                            SyntaxFactory.TriviaList
-                                                            (
-                                                                SyntaxFactory.Comment
-                                                                (
-                                                                    @"/*TODO: handle*/"
-                                                                )
-                                                            )
-                                                        )
-                                                    )
-                                                    .WithCloseBraceToken
-                                                    (
-                                                        SyntaxFactory.Token
-                                                        (
-                                                            SyntaxKind.CloseBraceToken
-                                                        )
-                                                    );
+                .WithOpenBraceToken(lambdaBodyToken)
+                .WithCloseBraceToken(SyntaxFactory.Token(SyntaxKind.CloseBraceToken));
             var lambdaExpression = SyntaxFactory.SimpleLambdaExpression(parameter, lambdaBody)
-                .WithArrowToken(SyntaxFactory.Token(SyntaxKind.EqualsGreaterThanToken));
-            var onErrorArgument = SyntaxFactory.Argument(onErrorNameColon, nopToken, lambdaExpression);
+                .WithArrowToken(
+                    SyntaxFactory.Token(SyntaxKind.EqualsGreaterThanToken)
+                        .WithLeadingTrivia(WhiteSpace)
+                        .WithTrailingTrivia(WhiteSpace));
+            var onErrorArgument = 
+                SyntaxFactory
+                .Argument(onErrorNameColon, nopToken, lambdaExpression)
+                .WithLeadingTrivia(WhiteSpace);
 
             return oldArguments.AddArguments(onErrorArgument);
         }
