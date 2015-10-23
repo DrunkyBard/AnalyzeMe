83d36645303922ce68094529713d09d31c093857
DrunkyBard <sattar.imamov@gmail.com>
2015-10-23 21:27:47 +0300

    - Code cleanup

--- src/AnalyzeMe/AnalyzeMe.Design.Analyzers/Utils/NodeFormatter.cs
+++ src/AnalyzeMe/AnalyzeMe.Design.Analyzers/Utils/NodeFormatter.cs
@@ -51,9 +51,9 @@ namespace AnalyzeMe.Design.Analyzers.Utils
                 : new SyntaxTrivia();
             var formattedComma = nextComma
                 .WithoutLeadingTrivia(SyntaxKind.WhitespaceTrivia)
-                .ReplaceTrivia(t => t.TrailingTrivia.LastOrDefault(x => x.IsKind(SyntaxKind.EndOfLineTrivia)), nextCommaLastTrailingTrivia)
                 .WithoutTrailingTrivia(SyntaxKind.WhitespaceTrivia)
-                .AddToTheTopTrailingTrivia(SyntaxFactory.Whitespace(" "));
+                .AddToTheTopTrailingTrivia(SyntaxFactory.Whitespace(" "))
+                .ReplaceTrivia(t => t.TrailingTrivia.LastOrDefault(x => x.IsKind(SyntaxKind.EndOfLineTrivia)), nextCommaLastTrailingTrivia);
 
             arguments = arguments
                 .ReplaceToken(
@@ -61,65 +61,10 @@ namespace AnalyzeMe.Design.Analyzers.Utils
                     arguments.OpenParenToken
                         .WithoutTrailingTrivia(SyntaxKind.EndOfLineTrivia, SyntaxKind.WhitespaceTrivia)
                         .AppendTrailingTrivia(openParenTrailingTrivia))
-                .ReplaceToken(n => n.Arguments[0].GetNextComma(), formattedComma)
+                .ReplaceNode(n => n.Arguments[0], arguments.Arguments[0].WithoutLeadingTrivia(SyntaxKind.WhitespaceTrivia))
                 .ReplaceNode(n => n.Arguments[0], arguments.Arguments[0].WithoutTrailingTrivia(SyntaxKind.WhitespaceTrivia, SyntaxKind.EndOfLineTrivia))
+                .ReplaceToken(n => n.Arguments[0].GetNextComma(), formattedComma)                
                 .ReplaceNode(n => n.Arguments[0], n => AlignWith(n, n.TryGetNextArgument()));
-
-            if (arguments.Arguments.Count > 1)
-            {
-                if (arguments.OpenParenToken.GetStartLinePosition() != arguments.Arguments[1].GetStartLinePosition() &&
-                    arguments.Arguments[1].GetPreviousComma().GetStartLinePosition() == arguments.Arguments[1].GetStartLinePosition())
-                {
-                    var secondArgumentWhitespaceLength = arguments.Arguments[1].ExtractWhitespace().Span.Length;
-                    formattedArgument = formattedArgument.WithoutTrivia(SyntaxKind.WhitespaceTrivia);
-                    var formattedArgumentLeadingTriviaLength = formattedArgument.GetLeadingTrivia().Span.Length;
-
-                    if (formattedArgumentLeadingTriviaLength < secondArgumentWhitespaceLength)
-                    {
-                        var newWhitespace = string.Concat(Enumerable.Repeat(" ", secondArgumentWhitespaceLength - formattedArgumentLeadingTriviaLength));
-                        var newLeadingTrivias = SyntaxFactory.TriviaList(
-                            formattedArgument.GetLeadingTrivia()
-                                .Union(SyntaxTriviaList.Create(SyntaxFactory.Whitespace(newWhitespace))));
-                        formattedArgument = formattedArgument.WithLeadingTrivia(newLeadingTrivias);
-                        var commaTrailingTrivia = arguments.Arguments[1].GetPreviousComma().TrailingTrivia;
-                        var eolComma = arguments.Arguments[1]
-                            .GetPreviousComma()
-                            .WithTrailingTrivia(
-                                SyntaxTriviaList.Create(SyntaxFactory.EndOfLine(Environment.NewLine)).Union(commaTrailingTrivia));
-                        arguments = arguments
-                            .ReplaceNode(n => n.Arguments[0], formattedArgument)
-                            .ReplaceToken(n => n.Arguments[1].GetPreviousComma(), eolComma);
-                    }
-                }
-                else
-                {
-                    formattedArgument = formattedArgument.WithoutFirstLeadingTrivia(SyntaxKind.WhitespaceTrivia);
-                    var newSecondArgumentSeparator =
-                        arguments.Arguments[1].GetPreviousComma()
-                            .WithoutFirstTrailingTrivia(SyntaxKind.WhitespaceTrivia)
-                            .WithTrailingTrivia(SyntaxFactory.Whitespace(" "));
-                    arguments = arguments
-                        .ReplaceNode(n => n.Arguments[0], formattedArgument)
-                        .ReplaceToken(
-                            t => t.OpenParenToken,
-                            arguments.OpenParenToken.WithoutFirstTrailingTrivia(SyntaxKind.WhitespaceTrivia))
-                        .ReplaceToken(
-                            t => t.Arguments[1].GetPreviousComma(),
-                            newSecondArgumentSeparator
-                        )
-                        .ReplaceNode(
-                            n => n.Arguments[1],
-                            arguments.Arguments[1].WithoutFirstLeadingTrivia(SyntaxKind.WhitespaceTrivia));
-                }
-            }
-            else
-            {
-                if (arguments.OpenParenToken.GetStartLinePosition() == formattedArgument.GetStartLinePosition())
-                {
-                    formattedArgument = formattedArgument.WithoutTrivia(SyntaxKind.WhitespaceTrivia);
-                    arguments = arguments.ReplaceNode(arguments.Arguments[0], formattedArgument);
-                }
-            }
 
             return arguments.Arguments[0];
         }
@@ -135,9 +80,9 @@ namespace AnalyzeMe.Design.Analyzers.Utils
             var firstArgumentLeadingTriviaLength = argument.GetLeadingTrivia().Span.Length;
             var secondArgumentWhitespaceLength = alignedArgument.Value.ExtractWhitespace().Span.Length;
 
-            if (firstArgumentLeadingTriviaLength > secondArgumentWhitespaceLength)
+            if (firstArgumentLeadingTriviaLength > secondArgumentWhitespaceLength) // For case when formatted argument contains comment leading trivia, etc.
             {
-                return argument; // For case when formatted argument contains comment leading trivia, etc.
+                return argument; 
             }
 
             var leadingWhitespace = string.Concat(Enumerable.Repeat(" ", secondArgumentWhitespaceLength - firstArgumentLeadingTriviaLength));
@@ -145,7 +90,6 @@ namespace AnalyzeMe.Design.Analyzers.Utils
             return argument.AddLeadingTrivia(SyntaxFactory.Whitespace(leadingWhitespace));
         }
 
-        // Рассмотреть вариант: просто повторить все leading/trailing trivia предыдущих/последующих запятых/аргументов
         private static ArgumentSyntax FormatLast(ArgumentListSyntax arguments, ArgumentSyntax formattedArgument)
         {
             throw new NotImplementedException();
